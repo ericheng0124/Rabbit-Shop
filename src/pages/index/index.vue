80ee70dcd7ab14d062fea5265692072339e376ac
ericheng0124 <233347237@qq.com>
2025-07-24 17:49:06 +0800

    首页-完成前台分类组件的数据类型和渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,27 +1,42 @@
 <script setup lang="ts">
 import { onLoad } from '@dcloudio/uni-app'
 import CustomNavbar from './components/CustomNavbar.vue'
-import { getHomeBannerAPI } from '@/services/home'
+import { getHomeBannerAPI, getCategoryAPI } from '@/services/home'
 import { ref } from 'vue'
-import type { BannerItem } from '@/types/home'
+import type { BannerItem, CategoryItem } from '@/types/home'
+import CategoryPanel from './components/CategoryPanel.vue'
 
+// 获取首页轮播图方法
 const bannerList = ref<BannerItem[]>([])
-
 const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
   // console.log(res)
   bannerList.value = res.result
 }
 
+// 获取前台分类数据
+const categoryList = ref<CategoryItem[]>([])
+const getHomeCategoryData = async () => {
+  const res = await getCategoryAPI()
+  // console.log(res)
+  categoryList.value = res.result
+}
+
 onLoad(() => {
   getHomeBannerData()
+  getHomeCategoryData()
 })
 </script>
 
 <template>
   <CustomNavbar />
   <XtxSwiper :bannerList="bannerList" />
+  <CategoryPanel :categoryList="categoryList" />
   <view> index </view>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+Page {
+  background-color: #f7f7f7;
+}
+</style>
--- src/services/home.ts
+++ src/services/home.ts
@@ -1,14 +1,27 @@
 import { http } from '@/utils/http'
-import type { BannerItem } from '@/types/home'
+import type { BannerItem, CategoryItem } from '@/types/home'
 /**
- *
+ * @description 首页广告区域-小程序
  * @param {*} distributionSite integer 可选 广告区域展示位置1 为首页（默认值）2 为商品分类页 示例值:1
  */
 export const getHomeBannerAPI = (distributionSite = 1) => {
   return http<BannerItem[]>({
+    method: 'GET',
     url: '/home/banner',
     data: {
       distributionSite,
     },
+  })
+}
+
+/**
+ * @description 首页-前台分类-小程序
+ * GET
+ * /home/category/mutli
+ */
+export const getCategoryAPI = () => {
+  return http<CategoryItem[]>({
+    method: 'GET',
+    url: '/home/category/mutli',
   })
 }
