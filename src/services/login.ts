b3c6c7193b6ccf6e9fbec13221576a412a6f6996
ericheng0124 <233347237@qq.com>
2025-07-28 17:48:18 +0800

    登陆模块-完成保存登陆信息

---
+++ src/services/login.ts
@@ -0,0 +1,33 @@
+import type { LoginResult } from '@/types/member'
+import { http } from '@/utils/http'
+
+type LoginParams = {
+  code: string
+  encryptedData: string
+  iv: string
+}
+export const postLoginWxMinAPI = (data: LoginParams) => {
+  return http<LoginResult>({
+    method: 'POST',
+    url: '/login/wxMin',
+    data,
+  })
+}
+
+/**
+  小程序登录-内测版
+  POST
+  /login/wxMin/simple
+  该接口用于模拟微信小程序一键登录
+  @param phoneNumber 模拟手机号
+ */
+
+export const postLoginWxMinSimpleAPI = (phoneNumber: string) => {
+  return http<LoginResult>({
+    method: 'POST',
+    url: '/login/wxMin/simple',
+    data: {
+      phoneNumber,
+    },
+  })
+}
