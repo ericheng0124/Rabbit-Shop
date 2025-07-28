3462468739078152aa0e689560145e4d771a98df
ericheng0124 <233347237@qq.com>
2025-07-28 17:52:08 +0800

    使用测试手机号信息

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -28,7 +28,7 @@ const onGetphonenumber: UniHelper.ButtonOnGetphonenumber = async (event) => {
 
 // 模拟手机号快捷登录
 const onGetphonenumberSimple = async () => {
-  const res = await postLoginWxMinSimpleAPI('18186698922')
+  const res = await postLoginWxMinSimpleAPI('13535337057')
   // console.log(res)
   loginSuccess(res.result)
 }
